48398cfeee7860623c83a9a1473502d4223bec8a
MBEgroupData <60991222+MBEgroupData@users.noreply.github.com>
2023-03-19 11:45:24 -0400

    Add files via upload

---
+++ pixel_control.py
@@ -0,0 +1,15 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+r = (255, 0, 0)     # red
+o = (255, 128, 0)   # orange
+y = (255, 255, 0)   # yellow
+g = (0, 255, 0)     # green
+c = (0, 255, 255)   # cyan
+b = (0, 0, 255)     # blue
+
+
+
+while True:
